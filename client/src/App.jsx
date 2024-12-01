39522587575af34bcda056e8a775e071b7e1c953
Nagaraj Murgod <01fe19bee418@gmail.com>
2024-12-01 18:30:49 +0530

    front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,26 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+} from "react-router-dom";
 
 function App() {
   
   return (
     <>
-      <Register/>
+      <Router>
+        
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/profile" element={<Profile/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
